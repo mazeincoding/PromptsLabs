56fb803aea13e4d314094b1bace494af578e7aea
Maze Winther <innoworldgames@gmail.com>
2024-09-25 20:50:26 +0200

    Added spacing at the bottom of the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
-      <div className="flex flex-col gap-10">
+      <div className="flex flex-col gap-10 pb-10">
         <Hero />
         <Library prompts_to_show={2} />
       </div>
