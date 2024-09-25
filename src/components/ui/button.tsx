6218110a75ed9ab1be50b948f9c8ee7d94580556
Maze Winther <innoworldgames@gmail.com>
2024-09-25 14:26:26 +0200

    Changed button from using primary colors to accent on outline variant

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -14,7 +14,7 @@ const buttonVariants = cva(
         destructive:
           "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
         outline:
-          "border border-primary/20 text-primary bg-background shadow-sm hover:bg-primary/20",
+          "border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground",
         secondary:
           "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
